6ba9b4914896122b34c73b70c18f3f7687288fca
Yassine LAMBARKI <developpeurk@gmail.com>
2020-09-30 23:09:00 +0100

    Updates

---
+++ precache-manifest.24f03bd3db975fc07eab3b393edc9426.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fe9a3ea9c38ee07a7ae9a86b47f4b96",
+    "url": "/monsters-roldex/index.html"
+  },
+  {
+    "revision": "a005b05474611b599352",
+    "url": "/monsters-roldex/static/css/main.6333e865.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-roldex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roldex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a005b05474611b599352",
+    "url": "/monsters-roldex/static/js/main.84f6e839.chunk.js"
+  },
+  {
+    "revision": "0126494f8635bc611006",
+    "url": "/monsters-roldex/static/js/runtime-main.667a69d7.js"
+  }
+]);
